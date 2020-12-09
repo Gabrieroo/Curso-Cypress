0b06996c918d3f5bffc68a6273fb62474ece1d54
Gabrieroo <gab.bernardes@hotmail.com>
2020-12-09 16:56:20 -0300

    visit

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,10 @@
+///<reference types="cypress"/>
+
+describe('Cypress basics', () => {
+
+    it('Visitiar pagina e fazer uam assertiva no titulo', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        
+    });
+    
+});
