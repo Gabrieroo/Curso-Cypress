339444be93064aaca0f237cce55fc30acb38b5cf
Gabrieroo <gab.bernardes@hotmail.com>
2020-12-11 15:33:32 -0300

    ALERT WITH MOCK

---
+++ cypress/integration/alert.spec.js
@@ -0,0 +1,27 @@
+///<reference types="cypress"/>
+
+describe("Alerts", () => {
+    before(() => {
+      cy.visit("https://www.wcaquino.me/cypress/componentes.html");
+    });
+    beforeEach(() => {
+      cy.reload;
+    });
+    it('Alert', () => {
+        cy.get('#alert').click();
+        cy.on('window:alert', msg => {
+            console.log(msg);
+            expect(msg).to.be.equal('Alert Simples');
+        })
+    });
+    it.only('Alert com mock', () => {
+
+        const stub = cy.stub().as('alerta');
+        
+        cy.on('window:alert', stub);
+
+        cy.get('#alert').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Alert Simples');
+        });
+    });
+});
