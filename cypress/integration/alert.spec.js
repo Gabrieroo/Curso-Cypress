d37b462cb1c05f6e276b45c313347b8568c37260
Gabrieroo <gab.bernardes@hotmail.com>
2020-12-11 15:45:24 -0300

    confirm and deny

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -14,7 +14,7 @@ describe("Alerts", () => {
             expect(msg).to.be.equal('Alert Simples');
         })
     });
-    it.only('Alert com mock', () => {
+    it('Alert com mock', () => {
 
         const stub = cy.stub().as('alerta');
         
@@ -23,5 +23,36 @@ describe("Alerts", () => {
         cy.get('#alert').click().then(() => {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples');
         });
+    });
+
+    it.only('Confirm', () => {
+        cy.get('#confirm').click();
+
+        cy.on('window:confirm', msg => {
+            console.log(msg);
+            expect(msg).to.be.equal('Confirm Simples');
+        })
+
+        cy.on('window:alert', msg => {
+            console.log(msg);
+            expect(msg).to.be.equal('Confirmado');
+        })
+        
+    });
+
+    it.only('deny', () => {
+        cy.get('#confirm').click();
+
+        cy.on('window:confirm', msg => {
+            console.log(msg);
+            expect(msg).to.be.equal('Confirm Simples');
+            return false;
+        })
+
+        cy.on('window:alert', msg => {
+            console.log(msg);
+            expect(msg).to.be.equal('Negado');
+        })
+        
     });
 });
