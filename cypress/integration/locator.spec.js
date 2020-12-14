191df58b101cd67756da6f5799f3ce9b7ef85b03
Gabrieroo <gab.bernardes@hotmail.com>
2020-12-14 13:54:18 -0300

    xpath

--- cypress/integration/locator.spec.js
+++ cypress/integration/locator.spec.js
@@ -11,6 +11,10 @@ describe("Locator", () => {
     cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3) > input').click();
     cy.get('[onclick*="Francisco"]').click();
   });
-  
+
+  it.only('Usando xpath', () => {
+      cy.xpath('//input');
+      cy.xpath('//*[@id="tabelaUsuarios"]/tbody/tr[2]/td[6]/input');
+  });
 
 });
