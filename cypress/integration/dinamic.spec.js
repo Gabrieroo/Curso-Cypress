2f352aaae326b6d8af8cdea05daaf5c7954ddade
Gabrieroo <gab.bernardes@hotmail.com>
2020-12-14 15:16:46 -0300

    testes dinamicos

---
+++ cypress/integration/dinamic.spec.js
@@ -0,0 +1,32 @@
+///<reference types="cypress"/>
+
+describe("teste dinamico", () => {
+    beforeEach(() => {
+    cy.visit("https://www.wcaquino.me/cypress/componentes.html");
+  });
+  beforeEach(() => {
+    cy.reload;
+  });
+  const foods = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
+  foods.forEach(food => {
+    it(`Cadastro com comida ${food}`, function () {
+     
+        cy.get('#formNome').type('Usuario');
+  
+        cy.get('#formSobrenome').type('Qualquer');
+    
+        cy.get(`[name=formSexo][value=F`).click()
+    
+        cy.xpath(`//label[contains(., '${food}')]/preceding-sibling::input`).click();
+    
+        cy.get('#formEscolaridade').select('Superior');
+    
+        cy.get('#formEsportes').select('Corrida');
+  
+        cy.get('#formCadastrar').click();
+  
+        cy.get("#resultado > :nth-child(1)").should("contain", "Cadastrado!");
+    });
+  })
+
+});
