be03f036479a6d487141d831327ca96cdaba57f5
Gabrieroo <gab.bernardes@hotmail.com>
2020-12-14 09:31:32 -0300

    Desafio cypress

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -1,91 +1,110 @@
 ///<reference types="cypress"/>
 
 describe("Alerts", () => {
-    before(() => {
-      cy.visit("https://www.wcaquino.me/cypress/componentes.html");
+  before(() => {
+    cy.visit("https://www.wcaquino.me/cypress/componentes.html");
+  });
+  beforeEach(() => {
+    cy.reload;
+  });
+  it("Alert", () => {
+    cy.get("#alert").click();
+    cy.on("window:alert", (msg) => {
+      console.log(msg);
+      expect(msg).to.be.equal("Alert Simples");
     });
-    beforeEach(() => {
-      cy.reload;
+  });
+  it("Alert com mock", () => {
+    const stub = cy.stub().as("alerta");
+
+    cy.on("window:alert", stub);
+
+    cy.get("#alert")
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith("Alert Simples");
+      });
+  });
+
+  it("Confirm", () => {
+    cy.get("#confirm").click();
+
+    cy.on("window:confirm", (msg) => {
+      console.log(msg);
+      expect(msg).to.be.equal("Confirm Simples");
     });
-    it('Alert', () => {
-        cy.get('#alert').click();
-        cy.on('window:alert', msg => {
-            console.log(msg);
-            expect(msg).to.be.equal('Alert Simples');
-        })
-    });
-    it('Alert com mock', () => {
+  });
 
-        const stub = cy.stub().as('alerta');
-        
-        cy.on('window:alert', stub);
+  it("deny", () => {
+    cy.get("#confirm").click();
 
-        cy.get('#alert').click().then(() => {
-            expect(stub.getCall(0)).to.be.calledWith('Alert Simples');
-        });
-    });
-
-    it('Confirm', () => {
-        cy.get('#confirm').click();
-
-        cy.on('window:confirm', msg => {
-            console.log(msg);
-            expect(msg).to.be.equal('Confirm Simples');
-        })
-
-        cy.on('window:alert', msg => {
-            console.log(msg);
-            expect(msg).to.be.equal('Confirmado');
-        })
-        
-    });
-
-    it('deny', () => {
-        cy.get('#confirm').click();
-
-        cy.on('window:confirm', msg => {
-            console.log(msg);
-            expect(msg).to.be.equal('Confirm Simples');
-            return false;
-        })
-
-        cy.on('window:alert', msg => {
-            console.log(msg);
-            expect(msg).to.be.equal('Negado');
-        })
-        
+    cy.on("window:confirm", (msg) => {
+      console.log(msg);
+      expect(msg).to.be.equal("Confirm Simples");
+      return false;
     });
 
-    it('prompt', () => {
-        // cy.get('#prompt').click();
-
-        // cy.on('window:prompt', msg => {
-        //     console.log(msg);
-        //     expect(msg).to.be.equal('Confirm Simples');
-        // })
-
-        // // cy.on('window:alert', msg => {
-        // //     console.log(msg);
-        // //     expect(msg).to.be.equal('Confirmado');
-        // // })
-        cy.window().then(win => {
-            cy.stub(win , 'prompt').returns('42')
-        }).as('prompt');
-
-        cy.get('#prompt').click();
-        
+    cy.on("window:alert", (msg) => {
+      console.log(msg);
+      expect(msg).to.be.equal("Negado");
     });
+  });
 
-    it.only('Desafio', () => {
-        cy.get('#formCadastrar').click()
+  it("prompt", () => {
+    // cy.get('#prompt').click();
 
-        const stub = cy.stub().as('alerta');
-        
-        cy.on('window:alert', stub);
-        console.log(stub);
-        
+    // cy.on('window:prompt', msg => {
+    //     console.log(msg);
+    //     expect(msg).to.be.equal('Confirm Simples');
+    // })
 
-        
-    });
+    // // cy.on('window:alert', msg => {
+    // //     console.log(msg);
+    // //     expect(msg).to.be.equal('Confirmado');
+    // // })
+    cy.window()
+      .then((win) => {
+        cy.stub(win, "prompt").returns("42");
+      })
+      .as("prompt");
 
+    cy.get("#prompt").click();
+  });
+
+  it.only("Desafio", () => {
+    const stub = cy.stub().as("alerta");
+
+    cy.on("window:alert", stub);
+
+    cy.get("#formCadastrar").click();
+
+    cy.get("#alert")
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith("Nome eh obrigatorio");
+      });
+    cy.get("#formNome").type("Teste usuario");
+
+    cy.get("#formCadastrar").click();
+    cy.get("#alert")
+      .click()
+      .then(() => {
+        expect(stub.getCall(2)).to.be.calledWith("Sobrenome eh obrigatorio");
+      });
+
+    cy.get("[data-cy=dataSobrenome]").type("teste sobrenome");
+
+    cy.get("#formCadastrar").click();
+
+    cy.get("#alert")
+      .click()
+      .then(() => {
+        expect(stub.getCall(4)).to.be.calledWith("Sexo eh obrigatorio");
+      });
+
+    cy.get("#formSexoMasc").click();
+    
+    cy.get("#formCadastrar").click();
+    cy.get("#resultado > :nth-child(1)").should("contain", "Cadastrado!");
+  });
 });
