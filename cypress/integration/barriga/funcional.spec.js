af1bad92796dd389168d6b4b38166cab5e986752
Gabrieroo <gab.bernardes@hotmail.com>
2020-12-15 11:09:44 -0300

    locators

--- cypress/integration/barriga/funcional.spec.js
+++ cypress/integration/barriga/funcional.spec.js
@@ -1,5 +1,7 @@
 ///<reference types="cypress"/>
 
+import loc from '../../support/locators'
+
 describe("Testes funcionais de uma aplicacao real", () => {
   before(() => {
     cy.visit("https://barrigareact.wcaquino.me", { timeout: 300000 });
@@ -11,20 +13,20 @@ describe("Testes funcionais de uma aplicacao real", () => {
       "barrigareact.wcaquino.me/login"
     );
 
-    cy.get('[data-test="email"]')
+    cy.get(loc.LOGIN.USER)
       .type("gabriel@teste.com")
       .should("have.value", "gabriel@teste.com");
-    cy.get('[data-test="passwd"]')
+    cy.get(loc.LOGIN.PASSWORD)
       .type("123456")
       .should("have.value", "123456");
-    cy.get(".btn", { timeout: 300000 }).click();
+    cy.get(loc.LOGIN.BTN_LOGIN, { timeout: 300000 }).click();
 
     cy.url({ timeout: 300000 }).should("include", "barrigareact.wcaquino.me");
 
-    cy.get(".toast-message",{ timeout: 300000 }).should('contain', 'Bem vindo');
+    cy.get(loc.MESSAGE,{ timeout: 300000 }).should('contain', 'Bem vindo');
   });
   it("Inserir conta", () => {
-    cy.get(".dropdown-toggle", { timeout: 300000 }).click();
+    cy.get(loc.MENU.SETTINGS, { timeout: 300000 }).click();
     cy.get('[href="/contas"]', { timeout: 300000 }).click();
 
     cy.url({ timeout: 300000 }).should(
@@ -34,21 +36,25 @@ describe("Testes funcionais de uma aplicacao real", () => {
 
     cy.get("h2", { timeout: 300000 }).should("have.text", "Contas");
 
-    cy.get(".form-control", { timeout: 300000 })
+    cy.get(loc.CONTAS.NOME, { timeout: 300000 })
       .type("Joao barbosa")
       .should("have.value", "Joao barbosa");
 
 
-    cy.get(".btn", { timeout: 300000 }).click();
+    cy.get(loc.CONTAS.BTN_SALVAR, { timeout: 300000 }).click();
 
-    cy.get(".toast-message",{ timeout: 300000 }).should('contain', 'Conta inserida com sucesso');
+    cy.get(loc.MESSAGE,{ timeout: 300000 }).should('contain', 'Conta inserida com sucesso');
 
 
   });
 
   it("alterar conta", () => {
-      cy.xpath("//table//td[contains(., 'Joao barbosa')]/..//i[@class='far fa-edit']", { timeout: 300000 }).click();
-      cy.get('[data-test=nome]').clear().type('João barboso');
+      cy.xpath(loc.CONTAS.BTN_ALTERAR_SELECT, { timeout: 300000 }).click();
+      cy.get(loc.CONTAS.NOME).clear().type('João barboso alterado');
+
+      cy.get(loc.CONTAS.BTN_SALVAR, { timeout: 300000 }).click();
+
+      cy.get(loc.MESSAGE,{ timeout: 300000 }).should('contain', 'Conta atualizada com sucesso!');
 
   });
 });
---
+++ cypress/support/locators.js
@@ -0,0 +1,19 @@
+const locators = {
+    LOGIN: {
+        USER: '[data-test="email"]',
+        PASSWORD: '[data-test="passwd"]',
+        BTN_LOGIN: '.btn'
+    },
+    MENU : {
+        SETTINGS : '.dropdown-toggle',
+        CONTAS : '[href="/contas"]'
+    },
+    CONTAS : {
+        NOME : '.form-control',
+        BTN_SALVAR: '.btn',
+        BTN_ALTERAR_SELECT : "//table//td[contains(., 'Joao barbosa')]/..//i[@class='far fa-edit']"
+    },
+    MESSAGE :  '.toast-message'
+
+}
+export default locators;
